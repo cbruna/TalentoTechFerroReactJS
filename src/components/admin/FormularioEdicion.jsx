e48278c0854849df1dacd7a019704d050c5bf0c4
cbruna <67879261+cbruna@users.noreply.github.com>
2025-07-01 16:38:22 -0300

    Update FormularioEdicion.jsx

--- src/components/admin/FormularioEdicion.jsx
+++ src/components/admin/FormularioEdicion.jsx
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 function FormularioEdicion({ productoSeleccionado, onActualizar }) {
     const [producto, setProducto] = useState(productoSeleccionado);
 
-    useEffect(()=>{
+    useEffect(() => {
         setProducto(productoSeleccionado)
-    },[productoSeleccionado])
+    }, [productoSeleccionado])
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -13,83 +13,87 @@ function FormularioEdicion({ productoSeleccionado, onActualizar }) {
 
     };
     return (
-        <form onSubmit={(e)=>{
+        <form onSubmit={(e) => {
             e.preventDefault()
             onActualizar(producto)
         }}>
-            <h2>Editar Producto</h2>
-            <div>
-                <label>ID</label>
-                <input
-                    type="number"
-                    name="id"
-                    value={producto.id || ''}
-                    onChange={handleChange}
-                    readOnly
-                />
-            </div>
-            <div>
-                <label>Nombre</label>
-                <input
-                    type="text"
-                    name="nombre"
-                    value={producto.nombre || ''}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div>
-                <label>Precio</label>
-                <input
-                    type="number"
-                    name="precio"
-                    value={producto.precio || ''}
-                    onChange={handleChange}
-                    required
-                    min="0"
-                />
-            </div>
-            <div>
-                <label>stock</label>
-                <input
-                    type="number"
-                    name="stock"
-                    value={producto.stock || ''}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div>
-                <label>Imagen URL</label>
-                <input
-                    type="text"
-                    name="imagen"
-                    value={producto.imagen || ''}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div>
-                <label>Categoria</label>
-                <input
-                    type="text"
-                    name="categoria"
-                    value={producto.categoria || ''}
-                    onChange={handleChange}
-                    required
-                />
-            </div>
-            <div>
-                <label>Descripción</label>
-                <input
-                    type="text"
-                    name="descripcion"
-                    value={producto.descripcion || ''}
-                    onChange={handleChange}
-                    
-                />
-            </div>
-            <button type="submit">Actualizar Producto</button>
+            <p>
+                <h2>Editar Producto</h2>
+                <div>
+                    <label>ID</label>
+                    <input
+                        type="number"
+                        name="id"
+                        value={producto.id || ''}
+                        onChange={handleChange}
+                        readOnly
+                    />
+                </div>
+                <div>
+                    <label>Nombre</label>
+                    <input
+                        type="text"
+                        name="nombre"
+                        value={producto.nombre || ''}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Precio</label>
+                    <input
+                        type="number"
+                        name="precio"
+                        value={producto.precio || ''}
+                        onChange={handleChange}
+                        required
+                        min="0"
+                    />
+                </div>
+                <div>
+                    <label>stock</label>
+                    <input
+                        type="number"
+                        name="stock"
+                        value={producto.stock || ''}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Imagen URL</label>
+                    <input
+                        type="text"
+                        name="imagen"
+                        value={producto.imagen || ''}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Categoria</label>
+                    <input
+                        type="text"
+                        name="categoria"
+                        value={producto.categoria || ''}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Descripción</label>
+                    <input
+                        type="text"
+                        name="descripcion"
+                        value={producto.descripcion || ''}
+                        onChange={handleChange}
+
+                    />
+                </div>
+                <p>
+                    <button style={{ backgroundColor: '#007bff', color: 'white' }} type="submit">Actualizar Producto</button>
+                </p>
+            </p>
         </form>
     );
 }
